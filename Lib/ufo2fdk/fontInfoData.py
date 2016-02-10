1d5cdc6a98688fbf60b8db37ad9e9c4d44686204
Frederik Berlaen <frederik@typemytype.com>
2016-02-10 16:13:34 +0100

    negative openTypeOS2WinAscent is not valid
    see https://github.com/behdad/fonttools/issues/520

--- Lib/ufo2fdk/fontInfoData.py
+++ Lib/ufo2fdk/fontInfoData.py
@@ -139,12 +139,16 @@ def openTypeOS2WinAscentFallback(info):
     """
     Fallback to the maximum y value of the font's bounding box.
     If that is not available, fallback to *ascender*.
+    If the maximum y value is negative, fallback to 0 (zero).
     """
     font = info.getParent()
     if font is None:
-        return getAttrWithFallback(info, "ascender")
-    bounds = getFontBounds(font)
-    xMin, yMin, xMax, yMax = bounds
+        yMax = getAttrWithFallback(info, "ascender")
+    else:
+        bounds = getFontBounds(font)
+        xMin, yMin, xMax, yMax = bounds
+    if yMax < 0:
+        return 0
     return yMax
 
 def openTypeOS2WinDescentFallback(info):
