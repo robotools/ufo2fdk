995d6181abea102d2c706544b5d295d9c7fdd811
Frederik Berlaen <frederik@typemytype.com>
2023-12-05 13:52:24 +0100

    remove deprecated string prefix

--- documentation/source/conf.py
+++ documentation/source/conf.py
@@ -169,8 +169,8 @@ htmlhelp_basename = 'ufo2fdkdoc'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, document class [howto/manual]).
 latex_documents = [
-  ('index', 'ufo2fdk.tex', ur'ufo2fdk Documentation',
-   ur'Tal Leming', 'manual'),
+  ('index', 'ufo2fdk.tex', 'ufo2fdk Documentation',
+   'Tal Leming', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
